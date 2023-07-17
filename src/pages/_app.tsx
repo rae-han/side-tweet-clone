ce24a216255598740da4a596906b4ff5c6517d82
raehan <raehan900809@gmail.com>
2023-07-17 21:24:10 +0900

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import '@/styles/reset.css'
+
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+
+  return (
+    <Component {...pageProps} />
+  )
 }
+
+export default App;
